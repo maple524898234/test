e0baa6680953b42a87ea088a32a68cb3e585286e
guofeng <guofeng@111.com.cn>
2019-06-18 15:37:31 +0800

    mint

---
+++ src/index.js
@@ -0,0 +1,30 @@
+import Button from '../packages/button';
+import '../src/assets/font/iconfont.css';
+import merge from './utils/merge';
+
+const version = '2.2.13';
+const install = function(Vue, config = {}) {
+  if (install.installed) return;
+
+  Vue.component(Button.name, Button);
+  Vue.use(InfiniteScroll);
+  Vue.use(Lazyload, merge({
+    loading: require('./assets/loading-spin.svg'),
+    attempt: 3
+  }, config.lazyload));
+
+  Vue.$messagebox = Vue.prototype.$messagebox = MessageBox;
+  Vue.$toast = Vue.prototype.$toast = Toast;
+  Vue.$indicator = Vue.prototype.$indicator = Indicator;
+};
+
+// auto install
+if (typeof window !== 'undefined' && window.Vue) {
+  install(window.Vue);
+};
+
+module.exports = {
+  install,
+  version,
+  Button
+};
